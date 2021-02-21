6e618fb6e69117ee45429c8a1ec361e1466312f5
Caio Henrique <caionwebdev@gmail.com>
2021-02-21 00:17:14 -0300

    last commit

---
+++ script.js
@@ -0,0 +1,31 @@
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button')
+
+// Prompt to select media stream, pass to video element, then play
+
+async function selectMediaStrem() {
+    try {
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata = () => {
+            videoElement.play();
+        }
+    } catch (error) {
+        // catch error
+        console.log('error', error)
+    }
+}
+
+
+button.addEventListener('click', async () => {
+    // disable button
+    button.disabled = true;
+    // start picture in picture
+    await videoElement.requestPictureInPicture();
+    // reset button
+    button.disabled = false
+})
+
+
+selectMediaStrem();
+
